3aaa473e7344bdf032d552e50ceb3f9bc05a497d
Breno Sarkis <sarkisbreno@gmail.com>
2018-08-02 13:06:51 +0200

    GameShouldHave12Squares

---
+++ Kata.Minesweeper/Kata.Minesweeper/GameShould.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace Kata.Minesweeper
+{
+    [TestFixture]
+    public class GameShould
+    {
+        [Test]
+        public void Have12Squares()
+        {
+            var game = new Minesweeper();
+
+            Assert.That(game.Squares.Length, Is.EqualTo(12));
+        }
+    }
+
+    public class Minesweeper
+    {
+        public int[,] Squares = new int[4,3];
+    }
+}
