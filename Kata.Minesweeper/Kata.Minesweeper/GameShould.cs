45c33bb88d49ce00dc61db9fadb5a8e5b8a3ed35
Breno Sarkis <sarkisbreno@gmail.com>
2018-08-02 16:20:29 +0200

    GameShouldContainTwoMines

--- Kata.Minesweeper/Kata.Minesweeper/GameShould.cs
+++ Kata.Minesweeper/Kata.Minesweeper/GameShould.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System.Linq;
 
 namespace Kata.Minesweeper
 {
@@ -11,11 +12,42 @@ namespace Kata.Minesweeper
             var game = new Minesweeper();
 
             Assert.That(game.Squares.Length, Is.EqualTo(12));
+            Assert.That(game.Squares.GetLength(0), Is.EqualTo(4));
+            Assert.That(game.Squares.GetLength(1), Is.EqualTo(3));
+        }
+
+        [Test]
+        public void ContainTwoMines()
+        {
+            var game = new Minesweeper();
+
+            Assert.That(game.MinesLeft(), Is.EqualTo(2));
         }
     }
 
     public class Minesweeper
     {
-        public int[,] Squares = new int[4,3];
+        public string[,] Squares = new string[4,3];
+
+        public Minesweeper()
+        {
+            Squares[1, 2] = "*";
+            Squares[0, 0] = "*";
+        }
+
+        public int MinesLeft()
+        {
+            int minesCount = 0;
+
+            foreach (var square in Squares)
+            {
+                if (square == "*")
+                {
+                    minesCount++;
+                }
+            }
+
+            return minesCount;
+        }
     }
 }
